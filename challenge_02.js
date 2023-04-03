49316a2744ea7c9fb7527e1125cde39bde72a79a
ASK-DONE <knueppelalex@web.de>
2023-04-03 13:28:10 +0200

    Challege2 ak

--- challenge_02.js
+++ challenge_02.js
@@ -16,3 +16,42 @@ Alter 13-17 // Cola
 
 /* Getränke-Challenge */
 
+
+
+
+let name = prompt("Bitte geben Sie Ihren Namen ein:");
+let age = parseInt (prompt("Bitte geben Sie Ihr Alter ein:"));
+
+
+
+// age >=0 && age <=5   // Milch  --> age > 0 AND age < 6
+// age <=6 && age <=12  // Saft
+// age <=13 && age <=17 // Cola
+// age <=18  // Wein
+
+
+const cond = true;
+
+
+switch (cond) {
+    case (age >=0 && age <=5):
+        console.log(name + " trinkt Milch");
+        break;
+
+    case (age >=6 && age <=12):
+        console.log(name + " trinkt Saft");
+        break;
+
+    case (age >=13 && age <=17):
+        console.log(name + " trinkt Cola!");
+        break;
+
+    case (age >=18):
+        console.log(name + " trinkt Wein!");
+        break;
+    
+        default:
+        console.log(name + " trinkt vielleicht besser nix mehr");
+        break;
+    }
+
